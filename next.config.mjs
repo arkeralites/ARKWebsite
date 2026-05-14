44847986a366ecd2a2e970c29eddb44c4dca2682
priyjohn <priyjohn@gmail.com>
2026-05-14 11:37:50 +0200

    Fix Vercel deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.kerala.no' }],
+        destination: 'https://kerala.no/:path*',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     unoptimized: false,
   },
